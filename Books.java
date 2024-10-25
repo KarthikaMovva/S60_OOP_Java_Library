e3cf83da740713a3fd922682b0ecb7a4cb85b6c4
KarthikaMovva <karthikamovva8306@gmail.com>
2024-10-25 14:32:25 +0530

    removed setter functions

--- Books.java
+++ Books.java
@@ -14,24 +14,12 @@ abstract class Book {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     public int getYear() {
         return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
     }
 
     public abstract void displayInfo();
@@ -47,10 +35,6 @@ class FictionBook extends Book {
 
     public String getGenre() {
         return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
     }
 
     @Override
@@ -69,10 +53,6 @@ class NonFictionBook extends Book {
 
     public String getSubject() {
         return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
     }
 
     @Override
