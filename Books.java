b2d39fa50d98e722a1f8e356ed6d73404964d745
KarthikaMovva <karthikamovva8306@gmail.com>
2024-10-25 13:53:18 +0530

    implemented OOP concepts

---
+++ Books.java
@@ -0,0 +1,92 @@
+
+abstract class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public abstract void displayInfo();
+}
+
+class FictionBook extends Book {
+    private String genre;
+
+    public FictionBook(String title, String author, int year, String genre) {
+        super(title, author, year);
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Fiction Book: " + getTitle() + " by " + getAuthor() + " (" + getYear() + "), Genre: " + genre);
+    }
+}
+
+class NonFictionBook extends Book {
+    private String subject;
+
+    public NonFictionBook(String title, String author, int year, String subject) {
+        super(title, author, year);
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Non-Fiction Book: " + getTitle() + " by " + getAuthor() + " (" + getYear() + "), Subject: " + subject);
+    }
+}
+
+public class Library {
+    public static void main(String[] args) {
+        FictionBook fictionBook = new FictionBook("1984", "George Orwell", 1949, "Dystopian");
+        NonFictionBook nonFictionBook = new NonFictionBook("Sapiens", "Yuval Noah Harari", 2011, "History");
+
+        fictionBook.displayInfo();
+        nonFictionBook.displayInfo();
+    }
+}
